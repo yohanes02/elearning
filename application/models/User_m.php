1771f10e8eb3c23310f405214c5775dd3be96eac
Yohanes <ysirorus11@gmail.com>
2021-05-29 22:58:44 +0700

    update model user

--- application/models/User_m.php
+++ application/models/User_m.php
@@ -2,13 +2,19 @@
 class User_m extends CI_Model
 {
 
-  function __construct()
-  {
-    parent::__construct();
-  }
+	function __construct()
+	{
+		parent::__construct();
+	}
 
 
-  public function getUser($id)
-  {
-  }
+	public function getUser($id)
+	{
+	}
+
+	public function getProfile($id)
+	{
+		$this->db->where(['id' => $id]);
+		return $this->db->get("adm_user");
+	}
 }
