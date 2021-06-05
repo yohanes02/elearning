9eecc2f18070c2efd37b9068bdce593d43406c46
Yohanes <ysirorus11@gmail.com>
2021-06-05 14:45:57 +0700

    add table komentar

--- db/eeleen.sql
+++ db/eeleen.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 03, 2021 at 03:11 PM
+-- Generation Time: Jun 05, 2021 at 02:44 PM
 -- Server version: 5.7.33-0ubuntu0.16.04.1
 -- PHP Version: 5.6.40-50+ubuntu16.04.1+deb.sury.org+1
 
@@ -75,8 +75,9 @@ CREATE TABLE `adm_user` (
 
 INSERT INTO `adm_user` (`id`, `email`, `fullname`, `password`, `type`, `phone`, `address`, `picture`, `gender`, `birthdate`, `question_id`, `answer`, `created_date`) VALUES
 (14, 'lucy@gmail.com', 'Lucy Kanti Rahayu', '81dc9bdb52d04dc20036dbd8313ed055', 't', '012231344', 'DKI Jakarta', 'icon_camera_photo.png', 'm', '1990-06-02 00:00:00', 7, 'mengajar', '2021-06-02 19:39:26'),
-(15, 'yohanes@gmail.com', 'Yohanes', '81dc9bdb52d04dc20036dbd8313ed055', 's', '081934195500', 'Penggilingan', NULL, 'm', '2000-02-10 00:00:00', 8, 'Jakarta', '2021-06-02 19:40:00'),
-(16, 'yunus@gmail.com', 'Yunus Sugito', '81dc9bdb52d04dc20036dbd8313ed055', 's', '09123111123', 'Duren Sawit', NULL, 'm', '1998-05-13 00:00:00', 7, 'badminton', '2021-06-02 19:40:41');
+(15, 'yohanes@gmail.com', 'Yohanes', '81dc9bdb52d04dc20036dbd8313ed055', 's', '081934195500', 'Penggilingan', 'doctor.jpg', 'L', '2000-02-10 00:00:00', 8, 'Jakarta', '2021-06-02 19:40:00'),
+(16, 'yunus@gmail.com', 'Yunus Sugito', '81dc9bdb52d04dc20036dbd8313ed055', 's', '09123111123', 'Duren Sawit', NULL, 'm', '1998-05-13 00:00:00', 7, 'badminton', '2021-06-02 19:40:41'),
+(17, 'eva@gmail.com', 'Eva Novianti', '81dc9bdb52d04dc20036dbd8313ed055', 't', '091228', 'Jakarta', NULL, 'f', '1990-06-09 00:00:00', 5, 'ayam', '2021-06-03 18:08:42');
 
 -- --------------------------------------------------------
 
@@ -95,6 +96,13 @@ CREATE TABLE `cls_answer` (
   `grade` int(3) DEFAULT NULL,
   `status_answer` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cls_answer`
+--
+
+INSERT INTO `cls_answer` (`id`, `assignment_id`, `attachment`, `uploaded_date`, `cls_id`, `student_id`, `student_name`, `grade`, `status_answer`) VALUES
+(1, 8, '28_Mei.docx', '2021-06-04 11:31:40', 13, 15, 'Yohanes', NULL, 2);
 
 -- --------------------------------------------------------
 
@@ -120,9 +128,8 @@ CREATE TABLE `cls_assignment` (
 --
 
 INSERT INTO `cls_assignment` (`id`, `cls_id`, `title`, `desc`, `type`, `due_date`, `attachment`, `created_date`, `creator_id`, `creator_name`) VALUES
-(4, 13, 'Tugas 1', 'Segera Dikerjakan', 'tugas', '2021-06-05 22:08:00', 'Pertemuan_11_Kompensasi_part11.pdf', '2021-06-02 22:08:27', 1, 'Ana'),
-(5, 13, 'Tugas 2', 'Tolong dikerjakan', 'tugas', '2021-06-15 22:36:00', 'pemasukan_pengeluaran.xlsx', '2021-06-02 22:33:34', 1, 'Ana'),
-(6, 13, 'Tugas 3', 'Tolong dikerjakan', 'tugas', '2021-06-08 09:03:00', 'Utsjosuakeren.docx', '2021-06-03 09:03:11', 1, 'Ana');
+(8, 13, 'Tugas 1', 'Tolong kerjakan dan perhatikan tenggat waktu pengumpulan', 'tugas', '2021-06-05 17:47:00', 'RPS-Semester_Ganjil_2020_2021.docx', '2021-06-03 17:47:54', 14, 'Lucy Kanti Rahayu'),
+(9, 13, 'Tugas 2', 'Tolong Dikerjakan', 'tugas', '2021-06-04 11:34:00', 'Daftar_Kelas.pdf', '2021-06-04 11:33:22', 14, 'Lucy Kanti Rahayu');
 
 -- --------------------------------------------------------
 
@@ -140,6 +147,13 @@ CREATE TABLE `cls_info` (
   `creator_id` int(11) DEFAULT NULL,
   `creator_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `cls_info`
+--
+
+INSERT INTO `cls_info` (`id`, `cls_id`, `title`, `desc`, `attachment`, `created_date`, `creator_id`, `creator_name`) VALUES
+(1, 13, 'PengumumanUAS AKAN DILAKSANAKAN PADA 30 JUNI 2021', '<p><strong>Pengumuman</strong><br /><br />UAS AKAN DILAKSANAKAN PADA <strong>30 JUNI 2021</strong></p>', NULL, '2021-06-03 15:42:10', 14, 'Lucy Kanti Rahayu');
 
 -- --------------------------------------------------------
 
@@ -162,7 +176,8 @@ CREATE TABLE `cls_main` (
 --
 
 INSERT INTO `cls_main` (`id`, `code`, `name`, `desc`, `owner_id`, `owner_name`, `created_date`) VALUES
-(13, 'faksl', 'E-Learning Malam', 'UNSADA SI 2020/2021', 14, 'Lucy Kanti Rahayu', '2021-06-02 19:42:38');
+(13, 'faksl', 'E-Learning Malam', 'UNSADA SI 2020/2021', 14, 'Lucy Kanti Rahayu', '2021-06-02 19:42:38'),
+(14, 'ntKoxV', 'Data Mining', 'UNSADA SI MALAM 2020/2021', 17, 'Eva Novianti', '2021-06-03 18:09:20');
 
 -- --------------------------------------------------------
 
@@ -182,7 +197,9 @@ CREATE TABLE `cls_participant` (
 --
 
 INSERT INTO `cls_participant` (`id`, `cls_id`, `student_id`, `join_date`) VALUES
-(6, 13, 15, '2021-06-02 19:43:03');
+(6, 13, 15, '2021-06-02 19:43:03'),
+(7, 14, 15, '2021-06-03 18:09:56'),
+(8, 13, 16, '2021-06-04 14:32:47');
 
 -- --------------------------------------------------------
 
@@ -207,6 +224,32 @@ CREATE TABLE `cls_subject` (
 
 INSERT INTO `cls_subject` (`id`, `cls_id`, `title`, `desc`, `attachment`, `created_date`, `creator_id`, `creator_name`) VALUES
 (4, 13, 'Materi 1', 'Tolong dipahami', 'Pertemuan_11_Kompensasi_part11.pdf', '2021-06-02 19:50:49', 14, 'Lucy Kanti Rahayu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentar`
+--
+
+CREATE TABLE `komentar` (
+  `id` int(11) NOT NULL,
+  `mtr_id` int(11) DEFAULT NULL,
+  `tgs_id` int(11) DEFAULT NULL,
+  `usr_id` int(11) NOT NULL,
+  `usr_name` varchar(255) NOT NULL,
+  `komentar` text NOT NULL,
+  `created_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `komentar`
+--
+
+INSERT INTO `komentar` (`id`, `mtr_id`, `tgs_id`, `usr_id`, `usr_name`, `komentar`, `created_date`) VALUES
+(1, NULL, 8, 15, 'Yohanes', 'Terima kasih', '0000-00-00 00:00:00'),
+(2, NULL, 8, 15, 'Yohanes', 'Terima Kasih 2', '0000-00-00 00:00:00'),
+(3, NULL, 8, 15, 'Yohanes', 'Terima Kasih 3', '0000-00-00 00:00:00'),
+(4, 4, NULL, 15, 'Yohanes', 'Terima Kasih', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -288,6 +331,12 @@ ALTER TABLE `cls_subject`
   ADD PRIMARY KEY (`id`) USING BTREE;
 
 --
+-- Indexes for table `komentar`
+--
+ALTER TABLE `komentar`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -300,36 +349,41 @@ ALTER TABLE `adm_question`
 -- AUTO_INCREMENT for table `adm_user`
 --
 ALTER TABLE `adm_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `cls_answer`
 --
 ALTER TABLE `cls_answer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `cls_assignment`
 --
 ALTER TABLE `cls_assignment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `cls_info`
 --
 ALTER TABLE `cls_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `cls_main`
 --
 ALTER TABLE `cls_main`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `cls_participant`
 --
 ALTER TABLE `cls_participant`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `cls_subject`
 --
 ALTER TABLE `cls_subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `komentar`
+--
+ALTER TABLE `komentar`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
